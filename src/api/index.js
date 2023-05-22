4f2dcf80410ae8635b48c17c50427748401cd674
bwesterg <benton.westergaard@gmail.com>
2023-05-22 15:32:13 -0600

    adds back GET method

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
 const options = {
-    // method: 'GET',
+    method: 'GET',
     // url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
     params: {
         bl_latitude: '11.847676',
